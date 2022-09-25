d40a887447d9a52a0aa37ddf520b9e77f3414ba2
Andrey Dmitriev <admit8490@gmail.com>
2022-09-25 10:18:50 +0200

    chore: added-categories-obj

--- helpers.js
+++ helpers.js
@@ -1,1 +1,12 @@
 // TODO: ADD Helper functions here
+
+export const forumCategoriesObj = {
+  299: { category: "Career Advice", className: "career" },
+  409: { category: "Project Feedback", className: "feedback" },
+  417: { category: "freeCodeCamp Support", className: "support" },
+  421: { category: "JavaScript", className: "javascript" },
+  423: { category: "HTML - CSS", className: "html-css" },
+  424: { category: "Python", className: "python" },
+  432: { category: "You Can Do This!", className: "motivation" },
+  560: { category: "Backend Development", className: "backend" },
+};
--- index.js
+++ index.js
@@ -4,3 +4,5 @@ import {
   FORUM_AVATARS,
   FORUM_API,
 } from "./constants.js";
+
+import { forumCategoriesObj } from "./helpers.js";
