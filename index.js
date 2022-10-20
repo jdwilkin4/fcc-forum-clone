583bb41dd58470b1640aafb984204256c53deb95
David Rosendorf <rosendorf.dev@gmail.com>
2022-10-20 12:35:58 +0000

    feat: forum fetch setup

--- index.js
+++ index.js
@@ -8,6 +8,31 @@ import {
 import { forumCategoriesObj } from "./helpers.js";
 
 const copyright = document.getElementById("copyright");
+const postsContainer = document.getElementById("posts-container");
+
+let isLoading = true;
+let isError = false;
+let forumData = null;
+
+// Fetch FCC forum latest data
+fetch(FORUM_API)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    forumData = data;
+    console.log("success");
+    console.log(forumData);
+  })
+  .catch((error) => {
+    isError = true;
+    console.log(error);
+  })
+  .finally(() => {
+    isLoading = false;
+  });
 
 copyright.innerText = new Date().getFullYear();
-const postsContainer = document.getElementById("posts-container");
