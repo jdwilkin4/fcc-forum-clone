9a479ef11a4a96619685e88a26535617df28a15a
David Rosendorf <rosendorf.dev@gmail.com>
2022-11-07 09:49:12 +0000

    #[41]-double-slash-in-urls

--- constants.js
+++ constants.js
@@ -1,4 +1,5 @@
-export const FORUM_TOPIC = "https://forum.freecodecamp.org/t/";
-export const FORUM_CATEGORY = "https://forum.freecodecamp.org/c/";
+// Do NOT include trailing slashes at the end of URLs in this project
+export const FORUM_TOPIC = "https://forum.freecodecamp.org/t";
+export const FORUM_CATEGORY = "https://forum.freecodecamp.org/c";
 export const FORUM_AVATARS = "https://sea1.discourse-cdn.com/freecodecamp";
 export const FORUM_API = "https://forum-proxy.freecodecamp.rocks/latest";
