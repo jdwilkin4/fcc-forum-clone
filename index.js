737ff75dcb82c04cd0b8a1c606fec09c9b80fb8e
David Rosendorf <rosendorf.dev@gmail.com>
2023-02-17 18:06:17 +0000

    small refactor, added loading state

--- index.js
+++ index.js
@@ -12,122 +12,119 @@ import {
   formatLargeNumber,
 } from "./helpers.js";
 
-const copyright = document.getElementById("copyright");
+// GLOBALS
 const postsContainer = document.getElementById("posts-container");
 const sortBtns = document.getElementsByName("sort");
 const categoryBtns = document.getElementById("filter-btns");
+const title = document.querySelector("main > h1");
 
 let isLoading = true;
 let isError = false;
 let forumData = null;
 let categories = new Map();
 
-// Fetch FCC forum latest data
-fetch(FORUM_API)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    forumData = data;
-    console.log("success");
-    console.log(forumData);
-    displayPostList();
-    displayCategories();
-    displayFooter();
-  })
-  .catch((error) => {
-    isError = true;
-    console.log(error);
-  })
-  .finally(() => {
-    isLoading = false;
-  });
-console.log(forumData);
+// MAIN
+document.addEventListener("DOMContentLoaded", () => {
+  // DOMContentLoaded event fires when the HTML document has been completely parsed
 
-const displayPostList = () => {
+  setLoadingState();
+  // Fetch FCC forum latest data
+  fetch(FORUM_API)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      forumData = data;
+      displayPostList();
+      displayCategories();
+      displayFooter();
+      activateSortBtns();
+    })
+    .catch((error) => {
+      isError = true;
+      console.log(error);
+    })
+    .finally(() => {
+      isLoading = false;
+    });
+});
+
+// AUXILIARY FUNCTIONS
+function displayPostList() {
   forumData["topic_list"].topics
     .filter((post) => post["category_id"] in supportedTopicCategories)
     .forEach(displayPost);
-};
 
-const displayPost = (post) => {
-  const category = supportedTopicCategories[post["category_id"]];
-  const posters = post.posters.map(({ user_id: userId }) => userId);
-  const users = forumData["users"];
+  function displayPost(post) {
+    const category = supportedTopicCategories[post["category_id"]];
+    const posters = post.posters.map(({ user_id: userId }) => userId);
+    const users = forumData["users"];
 
-  let postersAvatars = "";
-  const displayPosterAvatar = (posterId) => {
-    const poster = users.find((user) => user.id == posterId);
-    const avatarTemplate = poster["avatar_template"].replace("{size}", "25");
-    const posterAvatar = avatarTemplate.startsWith("/")
-      ? `${FORUM_AVATARS}/${avatarTemplate}`
-      : avatarTemplate;
-    postersAvatars += `
-      <a href="${FORUM_USER}/${poster.username}" target="_blank">
-        <img src="${posterAvatar}" title="${poster.username}" alt="Open ${poster.username}'s profile" width="25" height="25" />
-      </a>
-    `;
-  };
-  posters.forEach(displayPosterAvatar);
+    let postersAvatars = "";
+    const displayPosterAvatar = (posterId) => {
+      const poster = users.find((user) => user.id == posterId);
+      const avatarTemplate = poster["avatar_template"].replace("{size}", "25");
+      const posterAvatar = avatarTemplate.startsWith("/")
+        ? `${FORUM_AVATARS}/${avatarTemplate}`
+        : avatarTemplate;
+      postersAvatars += `
+          <a href="${FORUM_USER}/${poster.username}" target="_blank">
+            <img src="${posterAvatar}" title="${poster.username}" alt="Open ${poster.username}'s profile" width="25" height="25" />
+          </a>
+        `;
+    };
+    posters.forEach(displayPosterAvatar);
 
-  const ifSummaryDisplay = () => {
-    let summary = "";
-    if (post["has_summary"]) {
-      summary = `
-      <p class='post-summary'>
-        ${post.excerpt}
-        <a class='post-read-more' href='${FORUM_TOPIC}/${post.slug}' target='_blank'>read more</a>
-      </p>
-      `;
-    }
-    return summary;
-  };
+    const ifSummaryDisplay = () => {
+      let summary = "";
+      if (post["has_summary"]) {
+        summary = `
+          <p class='post-summary'>
+            ${post.excerpt}
+            <a class='post-read-more' href='${FORUM_TOPIC}/${post.slug}' target='_blank'>read more</a>
+          </p>
+          `;
+      }
+      return summary;
+    };
 
-  let postRow = `
-  <tr> 
-    <td class="post-topic">
-      <span>
-        <a class='post-title'
-           href='${FORUM_TOPIC}/${post.slug}'
-           target='_blank'>
-          ${post.title}
-        </a>
-      </span>
-      <div class='post-category'>
-        <a 
-          class='${category.name}'
-          href='${FORUM_CATEGORY}/${category.name}'
-          target='_blank'>
-          ${category.longName}
-        </a>
-      </div>
-      ${ifSummaryDisplay()}
-    </td>
-    <td class="post-posters">
-      <div class="postersAvatars">${postersAvatars}</div>
-    </td>
-    <td class="post-replies">${post.posts_count - 1}</td>
-    <td class="post-views">${formatLargeNumber(post.views)}</td>
-    <td class="post-activity">${formatDateDiff(Date.now(), post.bumped_at)}</td>
-  </tr>`;
-  postsContainer.innerHTML += postRow;
-};
-
-copyright.innerText = new Date().getFullYear();
-
-sortBtns.forEach((btn) => btn.addEventListener("click", handleSortBtnClick));
-
-function handleSortBtnClick(e) {
-  console.log(e.target.value);
+    let postRow = `
+      <tr> 
+        <td class="post-topic">
+          <span>
+            <a class='post-title'
+               href='${FORUM_TOPIC}/${post.slug}'
+               target='_blank'>
+              ${post.title}
+            </a>
+          </span>
+          <div class='post-category'>
+            <a 
+              class='${category.name}'
+              href='${FORUM_CATEGORY}/${category.name}'
+              target='_blank'>
+              ${category.longName}
+            </a>
+          </div>
+          ${ifSummaryDisplay()}
+        </td>
+        <td class="post-posters">
+          <div class="postersAvatars">${postersAvatars}</div>
+        </td>
+        <td class="post-replies">${post.posts_count - 1}</td>
+        <td class="post-views">${formatLargeNumber(post.views)}</td>
+        <td class="post-activity">${formatDateDiff(
+          Date.now(),
+          post.bumped_at
+        )}</td>
+      </tr>`;
+    postsContainer.innerHTML += postRow;
+  }
 }
 
-/**
- * Get categories of currently displayed topics
- * and display a button for each category with its respective count
- */
 function displayCategories() {
   // get categories and their counts
   forumData.topic_list.topics.forEach((topic) => {
@@ -153,4 +150,49 @@ function displayCategories() {
 
 function displayFooter() {
   document.getElementById("footer").style.display = "block";
+  document.getElementById("copyright").innerText = new Date().getFullYear();
+}
+
+function activateSortBtns() {
+  sortBtns.forEach((btn) => btn.addEventListener("click", handleSortBtnClick));
+
+  function handleSortBtnClick(e) {
+    console.log(e.target.value);
+  }
+}
+
+function setLoadingState() {
+  // let's make sure this code does not execute
+  // when the state is not set to isLoading = true
+  if (!isLoading) return;
+
+  let titleText = "Loading";
+  let titleUpdateInterval = null;
+  let dots = [];
+
+  title.innerHTML = titleText;
+  titleUpdateInterval = setInterval(updateTitle, 150);
+
+  function updateTitle() {
+    if (!isLoading || isError) {
+      // display appropriate title
+      if (isError) {
+        title.innerHTML = "Something went wrong :(";
+      } else {
+        title.innerHTML = "Latest topics";
+      }
+
+      // stop the title from changing to "Loading...""
+      clearInterval(titleUpdateInterval);
+      return;
+    }
+
+    if (dots.length < 3) {
+      dots.push(".");
+    } else {
+      dots = [];
+    }
+
+    title.innerHTML = titleText + dots.join("");
+  }
 }
